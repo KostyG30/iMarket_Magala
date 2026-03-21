4b97ab2e2f1b19474af1e52b4e9dcf253560e521
Kostya Hostiuk <kostelogostiuk98@gmail.com>
2026-03-21 19:10:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import telebot
-t_token = ""
+from telebot import types
+t_token = "6673322182:AAGnbL49OUiSL9sxL0j8ZsniOKFgLEmuqkE"
 bot = telebot.TeleBot(t_token)
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == '/start':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True) #create buttons
+        btn1 = types.KeyboardButton('Продуктові магазини')
+        btn2 = types.KeyboardButton('Все для дому')
+        btn3 = types.KeyboardButton('Іграшки')
+        btn4 = types.KeyboardButton('Про нас')
+        markup.add(btn1, btn2, btn3, btn4)
+        bot.send_message(message.from_user.id, 'Ласкаво просимо до мережі магазинів iMarket', reply_markup=markup)
+
+bot.polling(non_stop=True, interval=0)
