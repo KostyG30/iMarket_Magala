8f55566bbfde91cceeed0683e70e7467429878e1
Kostya Hostiuk <kostelogostiuk98@gmail.com>
2026-03-21 19:44:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ def get_text_messages(message):
     if message.text == '/start':
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True) #create buttons
         btn1 = types.KeyboardButton('Продуктові магазини')
-        btn2 = types.KeyboardButton('Все для дому')
-        btn3 = types.KeyboardButton('Іграшки')
+        btn2 = types.KeyboardButton('Для дому')
+        btn3 = types.KeyboardButton('Іграшкові магазини')
         btn4 = types.KeyboardButton('Про нас')
         markup.add(btn1, btn2, btn3, btn4)
         bot.send_message(message.from_user.id, 'Ласкаво просимо до мережі магазинів iMarket', reply_markup=markup)
-
+    elif message.text == 'Продуктові магазини':
+        bot.send_message(message.from_user.id, "Тут список продуктових магазинів.", parse_mode='Markdown')
 bot.polling(non_stop=True, interval=0)
