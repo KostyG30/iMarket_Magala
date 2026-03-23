4f2caae05c9e0e0d7a46df9bb3e81a9121cb2a8b
Kostya Hostiuk <kostelogostiuk98@gmail.com>
2026-03-23 19:52:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def get_text_messages(message):
         btn3 = types.KeyboardButton('Іграшки')
         btn4 = types.KeyboardButton('Про нас')
         markup.add(btn1, btn2, btn3, btn4)
-        bot.send_message(message.from_user.id, '👋 Привіт у iDelivery! 🚀' +
+        bot.send_message(message.from_user.id, '👋 Привіт у iMagala Delivery! 🚀' +
                          '\nХочеш швидку доставку?\nМи вже в дорозі! '
                          'Обирай товари — ми доставимо їх просто до тебе додому.'+ '\n' +
                          'iDelivery — замовив і вже чекаєш! 😎', reply_markup=markup)
