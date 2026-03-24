56a3f01de65884ef55db83f8266e94c2349f93ac
Kostya Hostiuk <kostelogostiuk98@gmail.com>
2026-03-24 17:16:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from telebot import types
 from apisetup import api
 t_token = api
 bot = telebot.TeleBot(t_token)
-@bot.message_handler(content_types=['/start'])
 
+@bot.message_handler(content_types=['/start'])
+def send_message(message):
+    pass
 
 
 bot.polling(non_stop=True, interval=0)
