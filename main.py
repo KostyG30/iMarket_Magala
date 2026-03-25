cf7c2c4d9fbbff69b70fc8b7f78452e3e4ed59f5
Kostya Hostiuk <kostelogostiuk98@gmail.com>
2026-03-25 11:02:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ bot = telebot.TeleBot(t_token)
 
 @bot.message_handler(commands=['/start'])
 def send_message(message):
-    markup = types.ReplyKeyboardMarkup()
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton('Продукти')
 
 
 bot.polling(non_stop=True, interval=0)
